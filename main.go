9f1b507b88c38511beecbfbe91a02b72c17f9ce9
Felix Burk <felix.burk@googlemail.com>
2024-09-17 06:05:47 +0200

    First test

--- main.go
+++ main.go
@@ -4,7 +4,34 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+type hunk struct {
+	preImage  lineIndex
+	postImage lineIndex
+
+	// e.g.
+	// diff --git a/file.txt b/file.txt
+	// index abcdef1..1234567 100644
+	// --- a/file.txt
+	// +++ b/file.txt
+	header string
+
+	// e.g.
+	// @@ -1,4 +1,4 @@
+	//  Line 1
+	// +Line 2
+	// -Line 3
+	content string
+}
+
+type lineIndex struct {
+	start         int
+	linesIncluded int
+}
 
 func main() {
 	input, err := io.ReadAll(os.Stdin)
@@ -13,9 +40,117 @@ func main() {
 		return
 	}
 
-	fmt.Println("Read all input:", ParsePatchFile(string(input)))
+	// normalize new lines
+	stdIn := strings.ReplaceAll(string(input), "\r\n", "\n")
+
+	hunks := SplitHunks(stdIn)
+
+	for _, h := range hunks {
+		converted, err := ConvertHunk(h, "abc")
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println(converted.Str())
+	}
 }
 
-func ParsePatchFile(content string) string {
-	return content
+func SplitHunks(content string) []hunk {
+	content = strings.Trim(content, "\n ")
+	idx := strings.Index(content, "@@")
+
+	if idx == -1 {
+		return []hunk{}
+	}
+
+	hunks := []hunk{}
+	currentHunkHeader := ""
+	inHunk := false
+	splitted := strings.Split(content, "\n")
+	for i, line := range splitted {
+		if i != len(splitted)-1 {
+			line += "\n"
+		}
+
+		newHunk := false
+		if strings.HasPrefix(line, "@@") {
+			newHunk = true
+			inHunk = true
+
+			re := regexp.MustCompile("[0-9]+")
+			digits := re.FindAllString(line, -1)
+
+			intSlice := make([]int, len(digits))
+			for i, digit := range digits {
+				intSlice[i], _ = strconv.Atoi(digit)
+			}
+
+			hunks = append(hunks, hunk{
+				header: strings.TrimSuffix(currentHunkHeader, "\n"),
+				preImage: lineIndex{
+					start:         intSlice[0],
+					linesIncluded: intSlice[1],
+				},
+				postImage: lineIndex{
+					start:         intSlice[2],
+					linesIncluded: intSlice[3],
+				},
+				content: "",
+			})
+			currentHunkHeader = ""
+		} else if !inHunk {
+			currentHunkHeader += line
+		}
+
+		if inHunk && !newHunk {
+			hunks[len(hunks)-1].content += line
+		}
+	}
+
+	return hunks
+}
+
+func ConvertHunk(h hunk, contains string) (hunk, error) {
+	// indexes are just preimage:
+	// linesIncluded = <content length> - <"+" lines>
+	// postimage: linesIncluded: <content length> - <"-" lines>
+
+	regex, err := regexp.Compile(contains)
+
+	if err != nil {
+		return hunk{}, fmt.Errorf("could not compile %s", contains)
+	}
+
+	newContent := ""
+	for _, line := range strings.Split(h.content, "\n") {
+		if regex.FindString(line) != "" {
+			if line[0] == '-' {
+				h.postImage.linesIncluded += 1
+			} else if line[0] == '+' {
+				h.postImage.linesIncluded -= 1
+			}
+
+			after := " " + line[1:]
+			newContent += "\n" + after
+		} else {
+			newContent += "\n" + line
+		}
+	}
+
+	h.content = strings.TrimPrefix(newContent, "\n")
+	return h, nil
+}
+
+func (h hunk) Str() string {
+	hunk := fmt.Sprintf(
+		"@@ -%d,%d +%d,%d @@\n%s",
+		h.preImage.start, h.preImage.linesIncluded,
+		h.postImage.start, h.postImage.linesIncluded,
+		h.content)
+	if h.header != "" {
+		hunk = fmt.Sprintf("%s\n%s", h.header, hunk)
+	}
+	return hunk
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,122 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestParsePatchFile(t *testing.T) {
-	input := "Test"
-	result := ParsePatchFile(input)
-
-	if result != input {
-		t.Errorf("wrong output %s for output %s", result, input)
+func TestHunkToString(t *testing.T) {
+	tests := []struct {
+		name     string
+		hunk     hunk
+		expected string
+	}{
+		{
+			name: "Simple Hunk",
+			hunk: hunk{
+				preImage: lineIndex{
+					start:         10,
+					linesIncluded: 5,
+				},
+				postImage: lineIndex{
+					start:         20,
+					linesIncluded: 5,
+				},
+				content: "-old line\n+new line",
+			},
+			expected: "@@ -10,5 +20,5 @@\n-old line\n+new line",
+		},
+		{
+			name: "Empty Content",
+			hunk: hunk{
+				preImage: lineIndex{
+					start:         30,
+					linesIncluded: 0,
+				},
+				postImage: lineIndex{
+					start:         40,
+					linesIncluded: 0,
+				},
+				content: "",
+			},
+			expected: "@@ -30,0 +40,0 @@\n",
+		},
+		{
+			name: "Multi-line Content",
+			hunk: hunk{
+				preImage: lineIndex{
+					start:         1,
+					linesIncluded: 3,
+				},
+				postImage: lineIndex{
+					start:         4,
+					linesIncluded: 3,
+				},
+				content: "-line 1\n-line 2\n+line 3\n+line 4",
+			},
+			expected: "@@ -1,3 +4,3 @@\n-line 1\n-line 2\n+line 3\n+line 4",
+		},
 	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := tt.hunk.Str()
+			if result != tt.expected {
+				t.Errorf("Hunk.ToString() = %v, expected %v", result, tt.expected)
+			}
+		})
+	}
+}
+
+func TestSplitHunks(t *testing.T) {
+	input := `diff --git a/file.txt b/file.txt
+index abcdef1..1234567 100644
+--- a/file.txt
++++ b/file.txt
+@@ -1,4 +1,4 @@
+ Line 1
+ Line 2
+ Line 3
+-Line 4
++Line 4 modified
+`
+
+	expectedResult := []hunk{{
+		preImage: lineIndex{
+			start:         1,
+			linesIncluded: 4,
+		},
+		postImage: lineIndex{
+			start:         1,
+			linesIncluded: 4,
+		},
+		header: `diff --git a/file.txt b/file.txt
+index abcdef1..1234567 100644
+--- a/file.txt
++++ b/file.txt`,
+		content: ` Line 1
+ Line 2
+ Line 3
+-Line 4
++Line 4 modified`},
+	}
+
+	result := SplitHunks(input)
+
+	if len(result) != len(expectedResult) {
+		t.Errorf("wrong result %+v for output %+v", result, input)
+	}
+	for i, s := range expectedResult {
+		if s != result[i] {
+			t.Errorf("wrong output at index %d was \n %+v but should be \n %+v",
+				i, result[i], s)
+		}
+	}
+
+}
+
+func DebugString(content string) string {
+	spaces := strings.ReplaceAll(content, " ", "_")
+	res := strings.ReplaceAll(spaces, "\n", "\\n")
+	return res
 }
